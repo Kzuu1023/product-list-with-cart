b80bd9b2ca83c36ed4402d47762eb79d949349c8
Kzuu1023 <delafiora552@gmail.com>
2025-01-07 00:36:19 -0800

    feat: improving BEMand add to cart logic

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const addToCart = document.querySelector(".button");
+const addToCart = document.querySelectorAll(".products__menu-item-button");
+const menuImg = document.querySelectorAll(".products__menu-item-img img");
+const changeAmount = document.querySelectorAll(".products__menu-item-amount");
 
-addToCart.addEventListener("click", function () {
-    document.querySelector(".amount").style.display = "flex";
-    addToCart.style.display = "none";
+addToCart.forEach((add, index) => {
+    add.addEventListener("click", function () {
+        const changeAmounts = changeAmount[index];
+        const menuImgs = menuImg[index];
+
+        changeAmounts.style.display = "flex";
+        add.style.display = "none";
+        changeAmounts.classList.add("selected");
+        menuImgs.style.border = "2px solid var(--red)";
+    });
 });
