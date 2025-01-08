080f1d1c511d5744e2a58e74ad3a524088553310
Kzuu1023 <delafiora552@gmail.com>
2025-01-08 05:21:45 -0800

    feat: price logic

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const addToCart = document.querySelectorAll(".products__menu-item-button");
 const menuImg = document.querySelectorAll(".products__menu-item-img img");
 const changeAmount = document.querySelectorAll(".products__menu-item-amount");
+let origPrice = document.querySelectorAll(".products__menu-item-price");
 let addAmount = document.querySelectorAll(
     ".products__menu-item-amount-increment"
 );
 let minusAmount = document.querySelectorAll(
     ".products__menu-item-amount-decrement"
 );
-let value = document.querySelectorAll(".products__menu-item-amount-value");
+let quantity = document.querySelectorAll(".products__menu-item-amount-value");
 
 addToCart.forEach((add, index) => {
     add.addEventListener("click", function () {
@@ -18,26 +19,39 @@ addToCart.forEach((add, index) => {
         add.style.display = "none";
         changeAmounts.classList.add("selected");
         menuImgs.style.border = "2px solid var(--red)";
-        updateQuantity();
     });
 });
 
 function updateQuantity() {
-    let quantity = 1;
+    let price = Array.from(origPrice).map((priceElement) =>
+        parseFloat(priceElement.innerText.replace(/[^0-9.-]+/g, ""))
+    );
 
+    // let price = parseFloat(
+    //     origPrice[index].innerText.replace(/[^0-9.-]+/g, "")
+    // );
+
+    let totalPrice = 0;
     addAmount.forEach((increment, index) => {
         increment.addEventListener("click", function () {
-            quantity += 1;
-            value[index].innerText = quantity;
+            let currentQuantity = parseFloat(quantity[index].innerText);
+            currentQuantity += 1;
+
+            quantity[index].innerText = currentQuantity;
+            totalPrice = currentQuantity * price[index];
+            console.log(totalPrice.toFixed(2));
         });
     });
 
     minusAmount.forEach((decrement, index) => {
         decrement.addEventListener("click", function () {
-            if (quantity > 0) {
-                quantity -= 1;
-                value[index].innerText = quantity;
+            let currentQuantity = parseFloat(quantity[index].innerText);
+            if (currentQuantity > 0) {
+                currentQuantity -= 1;
+                quantity[index].innerText = currentQuantity;
             }
         });
     });
 }
+
+updateQuantity();
