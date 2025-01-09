a3bda7244ca36b8093b063cf03fdd864c6770696
Kzuu1023 <delafiora552@gmail.com>
2025-01-09 06:38:36 -0800

    feat: display cart and total order

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const emptyCart = document.getElementById("cart__items");
+const itemCart = document.getElementById("cart__order");
 const addToCart = document.querySelectorAll(".products__menu-item-button");
 const menuImg = document.querySelectorAll(".products__menu-item-img img");
 const changeAmount = document.querySelectorAll(".products__menu-item-amount");
+const itemName = document.querySelectorAll(".products__menu-item-name");
+const orderName = document.querySelector(".cart__order-name");
+const orderDetails = document.querySelector(".cart__order-summary");
+const selectedQty = document.querySelector(".cart__order-selected-quantity");
+const selectedPrice = document.querySelector(".cart__order-selected-price");
+const selectedAmount = document.querySelector(".cart__order-selected-amount");
+const orderTotal = document.querySelector(".cart__order-total-order");
 let origPrice = document.querySelectorAll(".products__menu-item-price");
 let addAmount = document.querySelectorAll(
     ".products__menu-item-amount-increment"
@@ -9,53 +18,73 @@ let minusAmount = document.querySelectorAll(
     ".products__menu-item-amount-decrement"
 );
 let quantity = document.querySelectorAll(".products__menu-item-amount-value");
+let currentQuantity = Array.from(quantity).map(() => parseFloat(0));
+let price = Array.from(origPrice).map((priceElement) =>
+    parseFloat(priceElement.innerText.replace(/[^0-9.-]+/g, ""))
+);
 
-addToCart.forEach((addCart, index) => {
-    addCart.addEventListener("click", function () {
-        const changeAmounts = changeAmount[index];
-        const menuImgs = menuImg[index];
+function orderItem() {
+    addToCart.forEach((addCart, index) => {
+        addCart.addEventListener("click", function () {
+            const changeAmounts = changeAmount[index];
+            const menuImgs = menuImg[index];
 
-        changeAmounts.style.display = "flex";
-        addCart.style.display = "none";
-        changeAmounts.classList.add("selected");
-        menuImgs.style.border = "2px solid var(--red)";
+            changeAmounts.style.display = "flex";
+            addCart.style.display = "none";
+            changeAmounts.classList.add("selected");
+            menuImgs.style.border = "2px solid var(--red)";
+
+            emptyCart.style.display = "none";
+            itemCart.style.display = "flex";
+            displayCart(index);
+        });
     });
-});
 
-function updateQuantity() {
-    let totalPrice = 0;
-    let totalOrder = 0;
-    let price = Array.from(origPrice).map((priceElement) =>
-        parseFloat(priceElement.innerText.replace(/[^0-9.-]+/g, ""))
-    );
+    handleQuantityChange();
+}
 
+function handleQuantityChange() {
     addAmount.forEach((increment, index) => {
         increment.addEventListener("click", function () {
-            let currentQuantity = parseFloat(quantity[index].innerText);
-            currentQuantity += 1;
-            quantity[index].innerText = currentQuantity;
-
-            totalPrice = currentQuantity * price[index];
-            console.log(totalPrice.toFixed(2));
-            totalOrder = totalPrice;
-            console.log("The total amount: ", totalOrder.toFixed(2));
+            currentQuantity[index] += 1;
+            quantity[index].innerText = currentQuantity[index];
+            totalAmount();
         });
     });
 
     minusAmount.forEach((decrement, index) => {
         decrement.addEventListener("click", function () {
-            let currentQuantity = parseFloat(quantity[index].innerText);
-            if (currentQuantity > 0) {
-                currentQuantity -= 1;
-                quantity[index].innerText = currentQuantity;
-
-                totalPrice = totalPrice - price[index];
-                console.log(totalPrice.toFixed(2));
-                totalOrder = totalPrice;
-                console.log("The total amount: ", totalOrder.toFixed(2));
+            if (currentQuantity[index] > 0) {
+                currentQuantity[index] -= 1;
+                quantity[index].innerText = currentQuantity[index];
+                totalAmount();
             }
         });
     });
 }
 
-updateQuantity();
+function totalAmount() {
+    let totalPrice = 0;
+    let totalOrder = 0;
+    let calculatePrices = 0;
+
+    currentQuantity.forEach((quantity, index) => {
+        totalPrice = quantity * price[index];
+        calculatePrices += totalPrice;
+    });
+
+    totalOrder = calculatePrices;
+    console.log("The total amount: ", `$${totalOrder.toFixed(2)}`);
+}
+
+function displayCart(index) {
+    orderDetails.style.display = "flex";
+    orderName.innerText += itemName[index].innerText;
+    selectedQty.innerText += currentQuantity[index];
+    selectedPrice.innerText += `$${price[index].toFixed(2)}`;
+    selectedAmount.innerText += selectedPrice.innerText;
+    orderTotal.innerText = selectedAmount.innerText;
+    console.log("Current item selected: ", itemName[index].innerText);
+}
+
+orderItem();
